9bec783b76eb34397f536cb8214ddcf66bf5d5c4
Csaba Tóth <Csaba_Toth@epam.com>
2022-09-08 11:38:44 +0200

    DAL repostiory interface added

---
+++ ToDoApp.DataAccess/Interfaces/IToDoTaskRepository.cs
@@ -0,0 +1,11 @@
+﻿using ToDoApp.DTOs;
+
+namespace ToDoApp.DataAccess.Interfaces
+{
+    public interface IToDoTaskRepository
+    {
+        Task AddTaskAsync(ToDoTask task);
+        Task DeleteTaskAsync(int taskId);
+        Task UpdateTaskAsync(ToDoTask task);
+    }
+}
