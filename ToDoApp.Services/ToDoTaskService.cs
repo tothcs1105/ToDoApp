d51f582cefd6f36ec22d68ec321342a421384c89
Csaba Tóth <Csaba_Toth@epam.com>
2022-09-08 11:39:21 +0200

    Service implementation class added

---
+++ ToDoApp.Services/ToDoTaskService.cs
@@ -0,0 +1,29 @@
+﻿using ToDoApp.DataAccess.Interfaces;
+using ToDoApp.DTOs;
+using ToDoApp.Services.Interfaces;
+
+namespace ToDoApp.Services
+{
+    internal class ToDoTaskService : IToDoTaskService
+    {
+        public ToDoTaskService(IToDoTaskRepository toDoTaskRepository)
+        {
+
+        }
+
+        public Task<ToDoTask> AddTaskAsync(ToDoTask task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteTaskAsync(int taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ToDoTask> UpdateTaskAsync(ToDoTask task)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
