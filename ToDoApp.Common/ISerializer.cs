075c047493e65a8b493786e29c2a17044c3bc198
Csaba Tóth <Csaba_Toth@epam.com>
2022-09-09 10:59:37 +0200

    Common project added with json serializer

---
+++ ToDoApp.Common/ISerializer.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoApp.Common
+{
+    public interface ISerializer
+    {
+        string Serialize(object obj);
+
+        T Deserialize<T>(string serializedObj);
+    }
+}
---
+++ ToDoApp.Common/JsonSerializer.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace ToDoApp.Common
+{
+    public class JsonSerializer : ISerializer
+    {
+        public T Deserialize<T>(string serializedObj)
+        {
+            if(serializedObj == null)
+            {
+                throw new ArgumentNullException(nameof(serializedObj));
+            }
+
+            return JsonConvert.DeserializeObject<T>(serializedObj);
+        }
+
+        public string Serialize(object obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            return JsonConvert.SerializeObject(obj);
+        }
+    }
+}
