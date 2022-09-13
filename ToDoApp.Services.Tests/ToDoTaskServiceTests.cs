a1fe00a2a1054e28e1eece6732ea89e8178b39fc
Csaba Tóth <Csaba_Toth@epam.com>
2022-09-13 09:22:38 +0200

    Test added for AddTaskAsync

--- ToDoApp.Services.Tests/ToDoTaskServiceTests.cs
+++ ToDoApp.Services.Tests/ToDoTaskServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using Moq;
+﻿using FluentAssertions;
+using Moq;
 using Serilog;
 using ToDoApp.DataAccess.Interfaces;
 using ToDoApp.DTOs;
@@ -12,12 +13,27 @@ namespace ToDoApp.Services.Tests
         private Mock<ILogger> _loggerMock;
         private Mock<IToDoTaskRepository> _taskRepositoryMock;
         private Mock<ValidatorBase<ToDoTask>> _validatorMock;
+        private ToDoTaskService _sut;
 
         public ToDoTaskServiceTests()
         {
             _loggerMock = new Mock<ILogger>();
             _taskRepositoryMock = new Mock<IToDoTaskRepository>();
             _validatorMock = new Mock<ValidatorBase<ToDoTask>>();
+            _sut = new ToDoTaskService(_loggerMock.Object, _taskRepositoryMock.Object, _validatorMock.Object);
+        }
+
+        [Fact]
+        public async void AddTaskAsync_ArgumentNull_ShouldThrowArgumentNullException()
+        {
+            //Arrange
+            ToDoTask toDoTask = null;
+
+            //Act
+            Func<Task> act = async () => await _sut.AddTaskAsync(toDoTask);
+
+            //Assert
+            await act.Should().ThrowAsync<ArgumentNullException>();
         }
     }
 }
